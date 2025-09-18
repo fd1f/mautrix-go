0285bd7f970b4b86ecdbe3fcd85ffa665ac2b9e4
fd1f744993de14178e6c <30058021+fd1f@users.noreply.github.com>
2025-09-18 04:16:34 +0100

    filter: use []RelationType for not_aggregated_relations

--- filter.go
+++ filter.go
@@ -41,18 +41,18 @@ type RoomFilter struct {
 
 // FilterPart is used to define filtering rules for specific categories of events
 type FilterPart struct {
-	NotRooms                  []id.RoomID  `json:"not_rooms,omitempty"`
-	Rooms                     []id.RoomID  `json:"rooms,omitempty"`
-	Limit                     int          `json:"limit,omitempty"`
-	NotSenders                []id.UserID  `json:"not_senders,omitempty"`
-	NotTypes                  []event.Type `json:"not_types,omitempty"`
-	NotAggregatedRelations    []event.Type `json:"not_aggregated_relations,omitempty"`
-	Senders                   []id.UserID  `json:"senders,omitempty"`
-	Types                     []event.Type `json:"types,omitempty"`
-	ContainsURL               *bool        `json:"contains_url,omitempty"`
-	LazyLoadMembers           bool         `json:"lazy_load_members,omitempty"`
-	IncludeRedundantMembers   bool         `json:"include_redundant_members,omitempty"`
-	UnreadThreadNotifications bool         `json:"unread_thread_notifications,omitempty"`
+	NotRooms                  []id.RoomID          `json:"not_rooms,omitempty"`
+	Rooms                     []id.RoomID          `json:"rooms,omitempty"`
+	Limit                     int                  `json:"limit,omitempty"`
+	NotSenders                []id.UserID          `json:"not_senders,omitempty"`
+	NotTypes                  []event.Type         `json:"not_types,omitempty"`
+	NotAggregatedRelations    []event.RelationType `json:"not_aggregated_relations,omitempty"`
+	Senders                   []id.UserID          `json:"senders,omitempty"`
+	Types                     []event.Type         `json:"types,omitempty"`
+	ContainsURL               *bool                `json:"contains_url,omitempty"`
+	LazyLoadMembers           bool                 `json:"lazy_load_members,omitempty"`
+	IncludeRedundantMembers   bool                 `json:"include_redundant_members,omitempty"`
+	UnreadThreadNotifications bool                 `json:"unread_thread_notifications,omitempty"`
 }
 
 // Validate checks if the filter contains valid property values
